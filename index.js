23d976334613f026c66be592be555df5eb7f292d
rulyox <email@rulyox.com>
2019-08-17 15:41:36 +0900

    Execute python

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
 let express = require('express');
+let bodyParser = require('body-parser');
+let child_process = require('child_process');
+let fs = require('fs');
 
 let app = express();
+app.use(bodyParser.text());
 
-app.get('/', function(request, response) {
+app.get('/code', function(request, response) {
 
-    response.send('server online');
+    let code = request.body;
+
+    console.log(code);
+
+    let date = new Date();
+    let timestamp =
+        date.getFullYear().toString() +
+        ((date.getMonth() + 1) < 10 ? ('0' + (date.getMonth() + 1)) : (date.getMonth() + 1)).toString() +
+        (date.getDate() < 10 ? ('0' + date.getDate()) : date.getDate()).toString() +
+        (date.getHours() < 10 ? ('0' + date.getHours()) : date.getHours()).toString() +
+        (date.getMinutes() < 10 ? ('0' + date.getMinutes()) : date.getMinutes()).toString() +
+        (date.getSeconds() < 10 ? ('0' + date.getSeconds()) : date.getSeconds()).toString();
+    let location = __dirname + `/io/code/${timestamp}.py`;
+
+    // save code to file
+    fs.writeFile(location, code, function(err) {
+
+        if(err) return console.log(err);
+
+        // execute python
+        child_process.exec(
+            `python ${location}`,
+            function (error, stdout, stderr) {
+
+                if (error) {
+
+                    response.writeHead(200);
+                    response.end(error);
+
+                } else {
+
+                    response.writeHead(200);
+                    response.end(stdout);
+
+                }
+
+            });
+
+    });
 
 });
 
